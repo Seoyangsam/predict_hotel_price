c7cf1cf5d06cece750ee0bc7d0df584ecba646fa
seoyangsam <robert.xu@wur.nl>
2022-10-26 18:15:35 +0200

    initial commit

--- data-preprocessing.R
+++ data-preprocessing.R
@@ -2,8 +2,10 @@
 
 #First we read our datas
 
-train <- read.csv(file = 'data/train.csv')
+train <- read.csv(file = 'data/train.csv', header = TRUE, fileEncoding = 'latin1')
+str(train)
 head(train)
 
-test <- read.csv(file = 'data/test.csv')
+test <- read.csv(file = 'data/test.csv', header = TRUE, fileEncoding = 'latin1')
 head(test)
+
