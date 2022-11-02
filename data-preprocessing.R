71a556c391d8a26cc64c7f9f2dbd645601f77977
rubenmertens1 <ruben.mertens@ugent.be>
2022-11-02 10:08:10 +0100

    commit

--- data-preprocessing.R
+++ data-preprocessing.R
@@ -1,9 +1,14 @@
 #Here we perform the data preprocessing
 #First we read our datas
-train <- read.csv(file = 'data/train.csv', header = TRUE, fileEncoding = 'latin1')
+
+#install.packages("readr")
+install.packages('readr', dependencies = TRUE, repos='http://cran.rstudio.com/')
+library(readr)
+
+train <- read.csv(file = 'data/train.csv', header = TRUE, sep=",", fileEncoding = "latin1")
 str(train)
 
-test_X <- read.csv(file = 'data/test.csv', header = TRUE, fileEncoding = 'latin1')
+test_X <- read.csv(file = "data/test.csv", header = TRUE, sep=",", fileEncoding = "latin1")
 str(test_X)
 
 #Next, we split the independent & dependent variables in the training set.
@@ -11,7 +16,7 @@ train_X <- subset(train, select = -c(average_daily_rate))
 str(train_X)
 
 train_y <- train$average_daily_rate
-train_y <- gsub('  \u0080','',train_y) #remove the euro sign
+train_y <- gsub('  \u0080', '', train_y) #remove the euro sign
 train_y <- as.integer(train_y) #convert from chr to int
 str(train_y)
 
@@ -21,3 +26,11 @@ str(train_X)
 test_X <- subset(test_X, select = -c(booking_company, booking_agent))
 str(test_X)
 
+test_X$last_status_date <- as.Date(test_X$last_status_date)
+
+test_X$lead_time <- as.integer(sub("day(s)", "", test_X$lead_time))
+train_X$lead_time <- sub("day(s)","",train_X$lead_time)
+
+test_X$nr_booking_changes <- as.integer(test_X$nr_booking_changes)
+
+str(train_X)
