dbe87f2b79cf50c57fba4c252b8a94b2208d4520
hanne <Hanne.VanImpe@UGent.be>
2022-11-03 15:27:46 +0100

    update

--- src/feature_engineering.R
+++ src/feature_engineering.R
@@ -21,44 +21,31 @@ validation_X_ft_engineering <- validation_X_cleaned
 
 library(dummy)
 # get categories and dummies
-cats <- categories(train_X_ft_engineering[, c("assigned_room_type", "reserved_room_type", "booking_distribution_channel", "customer_type", "last_status", "market_segment", "meal_booked")])
+cats <- categories(train_X_ft_engineering[, c("assigned_room_type", "reserved_room_type", "country",  "booking_distribution_channel", "customer_type", "last_status", "market_segment", "meal_booked")], p=20)
 # apply on train set (exclude reference categories)
-dummies_train <- dummy(train_X_ft_engineering[, c("assigned_room_type", "reserved_room_type", "booking_distribution_channel", "customer_type", "last_status", "market_segment", "meal_booked")],
+dummies_train <- dummy(train_X_ft_engineering[, c("assigned_room_type", "reserved_room_type", "country", "booking_distribution_channel", "customer_type", "last_status", "market_segment", "meal_booked")],
                        object = cats)
 str(dummies_train)
-dummies_train <- subset(dummies_train, select = -c(assigned_room_type_P, reserved_room_type_P, booking_distribution_channel_Corporate, customer_type_Contract, last_status_Canceled, market_segment_Aviation, meal_booked_bed...breakfast..BB.))
+dummies_train <- subset(dummies_train, select = -c(assigned_room_type_P, reserved_room_type_P, country_Portugal, booking_distribution_channel_Corporate, customer_type_Contract, last_status_Canceled, market_segment_Aviation, meal_booked_bed...breakfast..BB.))
 # apply on test set (exclude reference categories)
-dummies_test <- dummy(test_X_ft_engineering[, c("assigned_room_type", "reserved_room_type", "booking_distribution_channel", "customer_type", "last_status", "market_segment", "meal_booked")],
+dummies_test <- dummy(test_X_ft_engineering[, c("assigned_room_type", "reserved_room_type", "country", "booking_distribution_channel", "customer_type", "last_status", "market_segment", "meal_booked")],
                        object = cats)
-dummies_test <- subset(dummies_test, select = -c(assigned_room_type_P, reserved_room_type_P, booking_distribution_channel_Corporate, customer_type_Contract, last_status_Canceled, market_segment_Aviation, meal_booked_bed...breakfast..BB.))
+dummies_test <- subset(dummies_test, select = -c(assigned_room_type_P, reserved_room_type_P, country_Portugal, booking_distribution_channel_Corporate, customer_type_Contract, last_status_Canceled, market_segment_Aviation, meal_booked_bed...breakfast..BB.))
 # apply on validation set (exclude reference categories)
-dummies_validation <- dummy(validation_X_ft_engineering[, c("assigned_room_type", "reserved_room_type", "booking_distribution_channel", "customer_type", "last_status", "market_segment", "meal_booked")],
+dummies_validation <- dummy(validation_X_ft_engineering[, c("assigned_room_type", "reserved_room_type", "country", "booking_distribution_channel", "customer_type", "last_status", "market_segment", "meal_booked")],
                        object = cats)
-dummies_validation <- subset(dummies_validation, select = -c(assigned_room_type_P, reserved_room_type_P, booking_distribution_channel_Corporate, customer_type_Contract, last_status_Canceled, market_segment_Aviation, meal_booked_bed...breakfast..BB.))
-
-#for country we select a subset of top 20 countries with highest frequency
-cats2 <- categories(train_X_ft_engineering[, c("country")], p = 20)
-# apply on train set (exclude reference categories)
-dummies_train2 <- dummy(train_X_ft_engineering[, c("country")],
-                       object = cats)
-dummies_train2 <- subset(dummies_train2, select = -c(country_Portugal))
-# apply on test set (exclude reference categories) and validation set
-dummies_test2 <- dummy(test_X_ft_engineering[, c("country")], object = cats)
-dummies_test2 <- subset(dummies_test2, select = -c(country_Portugal))
-
-dummies_validation2 <- dummy(validation_X_ft_engineering[, c("country")], object = cats)
-dummies_validation2 <- subset(dummies_validation2, select = -c(country_Portugal))
+dummies_validation <- subset(dummies_validation, select = -c(assigned_room_type_P, reserved_room_type_P, country_Portugal, booking_distribution_channel_Corporate, customer_type_Contract, last_status_Canceled, market_segment_Aviation, meal_booked_bed...breakfast..BB.))
 
 ## merge with overall training set
-train_X_ft_engineering <- subset(train_X_ft_engineering, select = -c(assigned_room_type, reserved_room_type, booking_distribution_channel, customer_type, last_status, market_segment, meal_booked))
+train_X_ft_engineering <- subset(train_X_ft_engineering, select = -c(assigned_room_type, reserved_room_type, country, booking_distribution_channel, customer_type, last_status, market_segment, meal_booked))
 train_X_ft_engineering <- cbind(train_X_ft_engineering, dummies_train)
 ## merge with overall test set
-test_X_ft_engineering <- subset(test_X_ft_engineering, select = -c(assigned_room_type, reserved_room_type, booking_distribution_channel, customer_type, last_status, market_segment, meal_booked))
+test_X_ft_engineering <- subset(test_X_ft_engineering, select = -c(assigned_room_type, reserved_room_type, country, booking_distribution_channel, customer_type, last_status, market_segment, meal_booked))
 test_X_ft_engineering <- cbind(test_X_ft_engineering, dummies_test)
 ## merge with overall validation set
-validation_X_ft_engineering <- subset(validation_X_ft_engineering, select = -c(assigned_room_type, reserved_room_type, booking_distribution_channel, customer_type, last_status, market_segment, meal_booked))
+validation_X_ft_engineering <- subset(validation_X_ft_engineering, select = -c(assigned_room_type, reserved_room_type, country, booking_distribution_channel, customer_type, last_status, market_segment, meal_booked))
 validation_X_ft_engineering <- cbind(validation_X_ft_engineering, dummies_train)
 
 #save the dataset
-write.table(train_X, file = "data/silver/train_X.csv", sep = "\t", row.names = F)
-write.table(test_X, file = "data/silver/test_X.csv", sep = "\t", row.names = F)
+write.table(train_X, file = "data/silver/train_X.csv", sep = ",", row.names = F)
+write.table(test_X, file = "data/silver/test_X.csv", sep = ",", row.names = F)
