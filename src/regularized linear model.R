b6ecec7a6982a86d1f506fa607cc8e575e2da4dd
seoyangsam <robert.xu@wur.nl>
2022-11-04 00:24:55 +0100

    model unfinished

--- src/regularized linear model.R
+++ src/regularized linear model.R
@@ -7,11 +7,11 @@ train_y <- read.csv(file = 'data/gold/train_y.csv', header = TRUE, fileEncoding 
 validation_y <- read.csv(file = 'data/gold/validation_y.csv', header = TRUE, fileEncoding = 'latin1')
 validation_X <- read.csv(file = 'data/gold/validation_X_scale.csv', header = TRUE, fileEncoding = 'latin1')
 
-train_X_data = data.frame(train_X,train_y)
+train_X_data <- data.frame(train_X,train_y)
 train_X_data$average_daily_rate <- train_X_data$x
 train_X_data <- subset(train_X_data, select = -c(x,arrival_date,last_status_date))
 
-validation_X_data = data.frame(validation_X,validation_y)
+validation_X_data <- data.frame(validation_X,validation_y)
 validation_X_data$average_daily_rate <- validation_X_data$x
 validation_X_data <- subset(validation_X_data, select = -c(x,arrival_date,last_status_date))
 str(validation_X_data)
@@ -33,4 +33,9 @@ bestlam.lasso <- cv.lasso$lambda.min
 pred.lasso <- predict(cv.lasso, s = bestlam.lasso, newx = validation_X_matrix)
 lasso.error <- mean((pred.lasso - validation_X_data$average_daily_rate)^2)
 coef.lasso <- predict(cv.lasso, type = "coefficients", s = bestlam.lasso)
+str(pred.lasso)
+pred_lasso <- data.frame(col1 = 1:16595, col2 = pred.lasso)
+str(pred_lasso)
+colnames(pred_lasso) <- c("id", "average_daily_rate")
+write.table(pred_lasso, file = "data/gold/pred_lasso.csv", sep = ",", row.names = FALSE, col.names=TRUE)
 
