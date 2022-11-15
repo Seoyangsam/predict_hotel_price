980f21ffc8814d86d11b9bc958c5b547167007ec
rubenmertens1 <ruben.mertens@ugent.be>
2022-11-15 22:12:00 +0100

    update

--- src/regularized linear model.R
+++ src/regularized linear model.R
@@ -9,10 +9,12 @@ validation_X <- read.csv(file = 'data/gold/validation_X_scale.csv', header = TRU
 test_set <- read.csv(file = 'data/gold/test_X_scale.csv', header = TRUE, fileEncoding = 'latin1')
 test_id <- read.csv(file = 'data/bronze/test_id.csv', header = TRUE, fileEncoding = 'latin1')
 
+
 # dependent and independent variables in 1 dataframe and delete arrival date and last status date
 train_X_data <- data.frame(train_X,train_y)
 train_X_data$average_daily_rate <- train_X_data$x
 train_X_data <- subset(train_X_data, select = -c(x,arrival_date,last_status_date))
+
 
 validation_X_data <- data.frame(validation_X,validation_y)
 validation_X_data$average_daily_rate <- validation_X_data$x
@@ -34,19 +36,23 @@ train_y_data <- subset(train_X_data, select= c(average_daily_rate))
 train_X_matrix <- model.matrix(lm.fit, train_X_data)
 test_set_matrix <- model.matrix(~., data = test_set)
 validation_X_matrix <- model.matrix(average_daily_rate ~., data = validation_X_data)
-
+colnames(validation_X_matrix)
 # fit a lasso regression model with CV
 grid <- 10 ^ seq(4, -2, length = 100)
 cv.lasso <- cv.glmnet(train_X_matrix, train_y_data$average_daily_rate ,alpha = 1, lambda = grid, nfolds = 5)
 bestlam.lasso <- cv.lasso$lambda.min
 
 # make predictions on test set
+
 pred.lasso.testset <- predict(cv.lasso, s = bestlam.lasso, newx = test_set_matrix )
+
+colnames(test_set)
 
 # make file with id and corresponding average daily rate
 lin_submission <- data.frame(col1 = test_id$x, col2 = pred.lasso.testset)
 
 colnames(lin_submission) <- c("id", "average_daily_rate")
 write.table(lin_submission, file = "data/results/lin_submission.csv", sep = ",", row.names = FALSE, col.names=TRUE)
+
 
 
