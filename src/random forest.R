202fef596b51bb9eb95458652dce78243adadc80
seoyangsam <robert.xu@wur.nl>
2022-12-05 15:48:38 +0100

    initial commit

--- src/random forest.R
+++ src/random forest.R
@@ -9,13 +9,23 @@ validation_X <- read.csv(file = 'data/silver/validation_X_cleaned.csv', header =
 train_X_data <- data.frame(train_X,train_y)
 str(train_X_data)
 
+cat <- categories(train_X_data[("country")], p = 15)
+str(cat)
+train_X_data$country[!(train_X_data$country %in% cat)] <- "others"
+str(train_X_data$country)
+
+# Convert all columns to factor
+train_X_data <- as.data.frame(unclass(train_X_data), stringsAsFactors = TRUE)
+
 #bagging
 #bag.train <- randomForest(average_daily_rate ~ ., data = train_X_data, mtry = 24, importance = TRUE)
 #yhat.bag <- predict(bag.train, newdata = validation_X)
 #mean((yhat.bag - validation_y)^2)
 
 #random forest
-rf.train <- randomForest(average_daily_rate ~ ., data = train_X_data, mtry = 6, importance = TRUE)
+rf.train <- randomForest(average_daily_rate ~ ., data = train_X_data, mtry = 6, importance = TRUE, ntree = 25)
 rf.train
 yhat.rf <- predict(rf.train, newdata = validation_X)
-mean((yhat.rf - validation_y)^2)
+yhat.rf
+validation_y
+mean((yhat.rf - validation_y$average_daily_rate)^2)
