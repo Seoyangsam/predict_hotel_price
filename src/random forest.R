c644f128bad9303ad92513b404040f0bc83d5077
seoyangsam <robert.xu@wur.nl>
2022-12-05 17:12:54 +0100

    initial commit

--- src/random forest.R
+++ src/random forest.R
@@ -1,6 +1,7 @@
 library(randomForest)
+library(dummy)
 set.seed(1)
-#read files
+# Read files
 train_X <- read.csv(file = 'data/silver/train_X_cleaned.csv', header = TRUE, fileEncoding = 'latin1')
 train_y <- read.csv(file = 'data/gold/train_y.csv', header = TRUE, fileEncoding = 'latin1')
 validation_y <- read.csv(file = 'data/gold/validation_y.csv', header = TRUE, fileEncoding = 'latin1')
@@ -9,23 +10,22 @@ validation_X <- read.csv(file = 'data/silver/validation_X_cleaned.csv', header =
 train_X_data <- data.frame(train_X,train_y)
 str(train_X_data)
 
+# Kepp top 15 values for country
 cat <- categories(train_X_data[("country")], p = 15)
-str(cat)
 train_X_data$country[!(train_X_data$country %in% cat)] <- "others"
 str(train_X_data$country)
 
+cat <- categories(validation_X[("country")], p = 15)
+validation_X$country[!(validation_X$country %in% cat)] <- "others"
+str(validation_X$country)
+
 # Convert all columns to factor
 train_X_data <- as.data.frame(unclass(train_X_data), stringsAsFactors = TRUE)
+validation_X <-as.data.frame(unclass(train_X_data), stringsAsFactors = TRUE,levels = levels(train_X_data))
 
-#bagging
-#bag.train <- randomForest(average_daily_rate ~ ., data = train_X_data, mtry = 24, importance = TRUE)
-#yhat.bag <- predict(bag.train, newdata = validation_X)
-#mean((yhat.bag - validation_y)^2)
-
-#random forest
-rf.train <- randomForest(average_daily_rate ~ ., data = train_X_data, mtry = 6, importance = TRUE, ntree = 25)
+# Random forest
+rf.train <- randomForest(average_daily_rate ~ ., data = train_X_data, mtry = 7, importance = TRUE, ntree = 15)
 rf.train
 yhat.rf <- predict(rf.train, newdata = validation_X)
-yhat.rf
-validation_y
 mean((yhat.rf - validation_y$average_daily_rate)^2)
+
