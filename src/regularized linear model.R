d64cfbce597766451a6421aef7be7d7488a6aea8
hanne <Hanne.VanImpe@UGent.be>
2022-12-08 16:16:02 +0100

    update

--- src/regularized linear model.R
+++ src/regularized linear model.R
@@ -1,3 +1,4 @@
+
 #here we do lasso regression
 library(glmnet)
 #read files
@@ -25,7 +26,6 @@ require(Matrix)
 train_y_data <- subset(train_X_data, select= c(average_daily_rate))
 
 train_X_matrix <- model.matrix(lm.fit, train_X_data)
-test_set_matrix <- model.matrix(~., data = test_set)
 validation_X_matrix <- model.matrix(average_daily_rate ~., data = validation_X_data)
 colnames(validation_X_matrix)
 # fit a lasso regression model with CV
@@ -33,10 +33,14 @@ grid <- 10 ^ seq(4, -2, length = 100)
 cv.lasso <- cv.glmnet(train_X_matrix, train_y_data$average_daily_rate ,alpha = 1, lambda = grid, nfolds = 5)
 bestlam.lasso <- cv.lasso$lambda.min
 
-# make predictions on test set
+# make predictions on validation set
 
-pred.lasso.testset <- predict(cv.lasso, s = bestlam.lasso, newx = test_set_matrix)
-sqrt(mean((pred.lasso.testset - validation_y$average_daily_rate)^2))
+pred.lasso.valset <- predict(cv.lasso, s = bestlam.lasso, newx = validation_X_matrix )
+str(pred.lasso.valset)
+pred_valset_error <- sqrt(mean((pred.lasso.valset - validation_y$average_daily_rate)^2))
+
+write.table(pred_valset_error, file = "data/results/linear_model_RMSE.csv", sep = ",", row.names = FALSE, col.names=TRUE)
+
 
 
 # SECOND STEP: RE-TRAIN ON TRAINING + VALIDATION SET AND PREDICT ON TEST SET
@@ -50,8 +54,8 @@ write.table(dependant_y, file = "data/gold/dependant_y.csv", sep = ",", row.name
 train_val_data <- data.frame(train_and_validation, dependant_y)
 
 # linear regression
-lm.fit <- lm(average_daily_rate ~ ., data = train_val_data)
-
+lm.fit2 <- lm(average_daily_rate ~ ., data = train_val_data)
+lm.fit2
 
 # prepare the data to be used with a lasso regression model
 library(Matrix)
@@ -59,22 +63,20 @@ require(Matrix)
 
 train_val_y_data <- subset(train_val_data, select= c(average_daily_rate))
 
-train_X_matrix <- model.matrix(lm.fit, train_val_data)
+train_X_matrix2 <- model.matrix(lm.fit2, train_val_data)
 test_set_matrix <- model.matrix(~., data = test_set)
 
 # fit a lasso regression model with CV
 grid <- 10 ^ seq(4, -2, length = 100)
-cv.lasso <- cv.glmnet(train_X_matrix, train_val_y_data$average_daily_rate ,alpha = 1, lambda = grid, nfolds = 5)
+cv.lasso <- cv.glmnet(train_X_matrix2, train_val_y_data$average_daily_rate ,alpha = 1, lambda = grid, nfolds = 5)
 bestlam.lasso <- cv.lasso$lambda.min
 
-# make predictions on validation set
+# make predictions on test set
 pred.lasso.testset <- predict(cv.lasso, s = bestlam.lasso, newx = test_set_matrix)
+
 
 # make file with id and corresponding average daily rate
 lin_submission <- data.frame(col1 = test_id$x, col2 = pred.lasso.testset)
 
 colnames(lin_submission) <- c("id", "average_daily_rate")
 write.table(lin_submission, file = "data/results/lin_submission.csv", sep = ",", row.names = FALSE, col.names=TRUE)
-
-
-
