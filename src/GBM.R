0794c6f7f42af0233a7bdd8079af6cb3c118c258
rubenmertens1 <ruben.mertens@ugent.be>
2022-12-11 21:01:33 +0100

    update

--- src/GBM.R
+++ src/GBM.R
@@ -24,7 +24,7 @@ model_gbm = gbm(average_daily_rate ~.,
                 data = train_X_data,
                 distribution = "gaussian",             #possibilities: gaussian,laplace,bernouilli,adaboost
                 cv.folds = 3,
-                shrinkage = .01,
+                shrinkage = 0.1,
                 n.minobsinnode = 10,
                 interaction.depth = 10,
                 n.trees = 3000)
@@ -62,3 +62,48 @@ final_gbm_submission <- data.frame(col1 = test_id$x, col2 = test_prediction)
 
 colnames(final_gbm_submission) <- c("id", "average_daily_rate")
 write.table(final_gbm_submission, file = "data/results/gbm_submission.csv", sep = ",", row.names = FALSE, col.names=TRUE)
+
+
+
+# read data
+train_X <- read.csv(file = 'data/gold/train_X_scale.csv', header = TRUE, fileEncoding = 'latin1')
+train_y <- read.csv(file = 'data/gold/train_y.csv', header = TRUE, fileEncoding = 'latin1')
+validation_y <- read.csv(file = 'data/gold/validation_y.csv', header = TRUE, fileEncoding = 'latin1')
+validation_X <- read.csv(file = 'data/gold/validation_X_scale.csv', header = TRUE, fileEncoding = 'latin1')
+test_set <- read.csv(file = 'data/gold/test_X_scale.csv', header = TRUE, fileEncoding = 'latin1')
+test_id <- read.csv(file = 'data/bronze/test_id.csv', header = TRUE, fileEncoding = 'latin1')
+
+# packages
+install.packages('gbm')                    # for fitting the gradient boosting model
+install.packages('caret')       # for general data preparation and model fitting
+
+library(gbm)
+library(caret)
+
+# create 1 dataframe
+train_X_data <- data.frame(train_X,train_y)
+validation_X_data <- data.frame(validation_X,validation_y)
+
+# FIRST STEP: TRAIN ON TRAINING SET AND PREDICT ON VALIDATION SET
+
+# tune parameters using cv k=5
+cv_5 = trainControl(method = "cv", number = 5)
+
+gbm_grid =  expand.grid(interaction.depth = 1:5,
+                        n.trees = (1:6) * 500,
+                        shrinkage = c(0.001, 0.01, 0.1),
+                        n.minobsinnode = 10)
+
+summary(gbm_grid)                    
+
+gbm_tune = train(average_daily_rate ~ ., data = train_X_data,
+                      method = "gbm",
+                      trControl = cv_5,
+                      verbose = FALSE,
+                      tuneGrid = gbm_grid)    
+                                          
+
+gbm_pred = predict(gbm_tune, newdata = validation_X, n.trees = )
+
+gbm.error <- sqrt(mean((gbm_pred - validation_y$average_daily_rate)^2))
+gbm.error
