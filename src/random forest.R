e56dfe042af3a1acf644705f9e69c7a0bc708aef
seoyangsam <robert.xu@wur.nl>
2022-12-19 17:45:46 +0100

    initial commit

--- src/random forest.R
+++ src/random forest.R
@@ -10,28 +10,6 @@ train_y <- read.csv(file = 'data/gold/train_y.csv', header = TRUE, fileEncoding 
 validation_y <- read.csv(file = 'data/gold/validation_y.csv', header = TRUE, fileEncoding = 'latin1')
 validation_X <- read.csv(file = 'data/silver/validation_X_cleaned.csv', header = TRUE, fileEncoding = 'latin1')
 
-# Make the dataframe to train the model
-#train_X<- data.frame(train_X,train_y)
-#validation_X<- data.frame(validation_X,validation_y)
-
-# Combine the training and validation sets
-#combined <- rbind(train_X, validation_X)
-
-# Create a vector of labels for stratified sampling
-labels <- as.factor(combined$assigned_room_type)
-
-# Split the combined dataset into a new training and validation set using stratified sampling
-#validation <- createDataPartition(labels, p = 0.3, list = FALSE)
-#validation_X1<- combined[validation, ]
-#train_X1 <- combined[-validation, ]
-
-#train_X <- subset(train_X1, select = -c(average_daily_rate))
-#validation_X <- subset(validation_X1, select = -c(average_daily_rate))
-
-#validation_y <- validation_X1$average_daily_rate
-#train_y <- train_X1$average_daily_rate
-#train_X_data <- data.frame(train_X,train_y)
-#str(train_X_data)
 # get all numeric columns for scaling
 scale_cols <- c("car_parking_spaces","lead_time","nr_adults","nr_children","nr_nights","special_requests", "nr_previous_bookings", "previous_cancellations")
 
@@ -43,17 +21,17 @@ train_X[, scale_cols] <- scale(train_X[, scale_cols], center = TRUE, scale = TRU
 # apply on validation set
 validation_X[, scale_cols] <- scale(validation_X[, scale_cols], center = mean_train, scale = sd_train)
 
+# Convert all columns to factor
+train_X <- as.data.frame(unclass(train_X), stringsAsFactors = TRUE)
+validation_X <-as.data.frame(unclass(validation_X), stringsAsFactors = TRUE,levels = levels(train_X))
+
 # Make the dataframe to train the model
 train_X_data <- data.frame(train_X,train_y)
-
-# Convert all columns to factor
-train_X_data <- as.data.frame(unclass(train_X_data), stringsAsFactors = TRUE)
-validation_X <-as.data.frame(unclass(validation_X), stringsAsFactors = TRUE,levels = levels(train_X_data))
 
 # Hyperparemeter tuning with mtry
 mtry_values <- c(sqrt(ncol(train_X)), ncol(train_X), 0.5*ncol(train_X))
 tunegrid <- expand.grid(mtry = mtry_values)
-rf_default <- train(train_y~.,
+rf_default <- train(average_daily_rate~.,
                       data=train_X_data,
                       method='rf',
                       metric="RMSE",
@@ -64,7 +42,7 @@ rf_default <- train(train_y~.,
 print(rf_default)
 
 # retrain Random forest
-updated_mtry <- expand.grid(mtry=4)
+updated_mtry <- expand.grid(mtry=sqrt(ncol(train_X)))
 rf.train <- train(average_daily_rate~.,
                       data=train_X_data,
                       method='rf',
@@ -76,48 +54,16 @@ rf.train <- train(average_daily_rate~.,
 print(rf.train)
 
 yhat.rf <- predict(rf.train, newdata = validation_X)
-
+df<-yhat.rf - validation_y
 #calculate the rmse
-rf_error <- sqrt(mean((yhat.rf - validation_y)^2))
+rf_error <- sqrt(mean((df$average_daily_rate)^2))
 rf_error
 
-mae <- mean(abs(yhat.rf - validation_y))
+mae <- mean(abs(df$average_daily_rate))
 mae
 
-#data_frame <- data.frame(rf_error = rf_error,
-  #mae = mae
-#)
-#write.table(data_frame, file = "data/results/rf_error.csv", sep = ",", row.names = FALSE, col.names=TRUE)
-
-write.table(rf_error, file = "data/results/rf_RMSE.csv", sep = ",", row.names = FALSE, col.names=TRUE)
-write.table(mae, file = "data/results/rf_MAE.csv", sep = ",", row.names = FALSE, col.names=TRUE)
+data_frame <- data.frame(rf_error = rf_error, mae = mae)
+write.table(data_frame, file = "data/results/rf_error.csv", sep = ",", row.names = FALSE, col.names=TRUE)
 
 
-# retrain Random forest
-set.seed(123)
-updated_mtry <- expand.grid(mtry=5)
-rf.train <- train(average_daily_rate~.,
-                      data=train_X_data,
-                      method='rf',
-                      metric="RMSE",
-                      tuneGrid=updated_mtry,
-                      min.node.size = 20,
-                      ntree=100)
-print(rf.train)
 
-yhat.rf <- predict(rf.train, newdata = validation_X)
-
-#calculate the rmse
-rf_error <- sqrt(mean((yhat.rf - validation_y)^2))
-rf_error
-
-mae <- mean(abs(yhat.rf - validation_y))
-mae
-
-#data_frame <- data.frame(rf_error = rf_error,
-  #mae = mae
-#)
-#write.table(data_frame, file = "data/results/rf_error.csv", sep = ",", row.names = FALSE, col.names=TRUE)
-
-write.table(rf_error, file = "data/results/rf_RMSE.csv", sep = ",", row.names = FALSE, col.names=TRUE)
-write.table(mae, file = "data/results/rf_MAE.csv", sep = ",", row.names = FALSE, col.names=TRUE)
