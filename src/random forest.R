383b8882817e781ca1ffef33322d00e4fa71b54d
seoyangsam <robert.xu@wur.nl>
2022-12-19 21:11:25 +0100

    initial commit

--- src/random forest.R
+++ src/random forest.R
@@ -1,7 +1,7 @@
+# Set seed value
 set.seed(1)
 
-# import libraris
-library(randomForest)
+# Import libraris
 library(caret)
 
 # Read files
@@ -10,15 +10,15 @@ train_y <- read.csv(file = 'data/gold/train_y.csv', header = TRUE, fileEncoding 
 validation_y <- read.csv(file = 'data/gold/validation_y.csv', header = TRUE, fileEncoding = 'latin1')
 validation_X <- read.csv(file = 'data/silver/validation_X_cleaned.csv', header = TRUE, fileEncoding = 'latin1')
 
-# get all numeric columns for scaling
+# Get all numeric columns for scaling
 scale_cols <- c("car_parking_spaces","lead_time","nr_adults","nr_children","nr_nights","special_requests", "nr_previous_bookings", "previous_cancellations")
 
-# apply on training set
+# Apply on training set
 mean_train <- colMeans(train_X[, scale_cols])
 sd_train <- sapply(train_X[, scale_cols], sd)
 train_X[, scale_cols] <- scale(train_X[, scale_cols], center = TRUE, scale = TRUE)
 
-# apply on validation set
+# Apply on validation set
 validation_X[, scale_cols] <- scale(validation_X[, scale_cols], center = mean_train, scale = sd_train)
 
 # Convert all columns to factor
@@ -41,7 +41,7 @@ rf_default <- train(average_daily_rate~.,
                       ntree=100)
 print(rf_default)
 
-# retrain Random forest
+# Retrain Random Forest with mtry = square root of number of predictors
 updated_mtry <- expand.grid(mtry=sqrt(ncol(train_X)))
 rf.train <- train(average_daily_rate~.,
                       data=train_X_data,
@@ -53,17 +53,19 @@ rf.train <- train(average_daily_rate~.,
                       ntree=100)
 print(rf.train)
 
+# Make new predicton on validation set
 yhat.rf <- predict(rf.train, newdata = validation_X)
 df <- yhat.rf - validation_y
 
-#calculate the rmse
+# Calculate the rmse
 rf_error <- sqrt(mean((df$average_daily_rate)^2))
 rf_error
 
-#calculate the mae
+# Calculate the mae
 mae <- mean(abs(df$average_daily_rate))
 mae
 
+# Store the result
 data_frame <- data.frame(rf_error = rf_error, mae = mae)
 write.table(data_frame, file = "data/results/rf_error.csv", sep = ",", row.names = FALSE, col.names=TRUE)
 
